91685f003cb4b5de9c71c67fd84e0843c7755bd4
jScriptural <isongjohn014@gmail.com>
2025-06-05 11:42:17 +0000

    completed ex3-2

--- chapter3/ex3-2/solution.c
+++ chapter3/ex3-2/solution.c
@@ -4,21 +4,27 @@
 #define N 256
 
 
-void escape(char *s,char *t);
+void escape(char *s,const char *t);
+void reverse_escape(const char *s, char *t);
 
 
 int main(void)
 {
-  char t[N] = "\tHello,\n\t\t World\n";
+  char t[N] = "Hello,\\n\t\nWorld\n";
   char s[N] = {0};
+  char v[N] = {0};
 
   escape(s,t);
+  reverse_escape(s, v);
 
-  printf("%s",s);
+  printf("t: %s",t);
+  printf("s: %s",s);
+  printf("\n*****************\n");
+  printf("v: %s",v);
 }
 
 
-void escape(char *s,char *t)
+void escape(char *s,const char *t)
 {
  
   int i= 0, j=0;
@@ -44,5 +50,36 @@ void escape(char *s,char *t)
     }//switch
 
   }//for
+   
+  s[j] = '\0'; //NUL terminated;
+}
 
+void reverse_escape(const char *s, char *t)
+{
+  size_t len = strlen(s);
+
+  for(int j=0, i = 0; i < len; ++i)
+  {
+
+    if(s[i] == 0x5c)
+    {
+      if(s[i+1] == 'n')
+      {
+	t[j++] = '\n';
+	++i;
+      }
+      else if(s[i+1] == 't')
+      {
+	t[j++] = '\t';
+	++i;
+      }
+      else
+	t[j++] = 0x5c;
+    }
+    else
+      t[j++] = s[i];
+
+  }//for
+
+  t[j] = '\0'; //NUL terminated
 }
