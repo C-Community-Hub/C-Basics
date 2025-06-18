616f46efab3671b19a09343150c6956e52d792d3
jScriptural <isongjohn014@gmail.com>
2025-06-18 19:05:49 +0000

    Added ex4-1

---
+++ chapter4/ex4-1/solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+ssize_t strindex(const char source[], const char searchFor[]);
+
+
+/* return the index of rightmost 
+ * occurrence of t in s.
+ */
+ssize_t  strindex(const char s[], const char t[])
+{
+  size_t len_s, len_t;
+
+  if(s == NULL || t == NULL)
+    return -1;
+
+
+  len_t = strlen(t);
+  len_s = strlen(s);
+
+  //if t is empty, match at end of s
+  if(len_t == 0)
+    return len_s-1;
+
+  for(ssize_t i = len_s - len_t; i >= 0; --i)
+  {
+    for(ssize_t j = len_t-1, k = i; j >= 0 && t[j] == s[k]; --k, --j)
+      if(j == 0)
+	return k;
+  }
+
+  return -1;
+}
